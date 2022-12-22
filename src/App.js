40a5fe26c1eabd59990a8e7d780e45778203ac8e
Contesini <gcontesini@gmail.com>
2022-12-22 09:46:11 -0500

    Cook Book

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return ( 
+    <div 
+      style={{
+        margin: "auto",
+        width: 800,
+        paddingTop: "1rem",
+      }}
+    >
+      <h1
+        className="title"
+      >
+        Main Title 
+      </h1>
+      <table 
+        width="100%"
+      >
+        <thead>
+          <tr>
+            <th>Recipe</th>
+            <th>Type</th>
+            <th
+              width="200%"
+            >
+              Link
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Chocolate Cake</td>
+            <td>Desert</td>
+            <td>
+              <a 
+                href="https://www.youtube.com/watch?v=UsyrMndrICQ&list=PLOc2A52hKlB6OTtLo0UQRKfd5-Fbn9iK2&index=341"
+                target="_blank">
+                yt-Link
+              </a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
