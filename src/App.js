20ca86aa3afb5131bc37e770c1cfc32304651d2f
Contesini <gcontesini@gmail.com>
2022-12-27 08:50:33 -0500

    recipes.json file

--- src/App.js
+++ src/App.js
@@ -1,50 +1,71 @@
 import React from "react";
 import './App.css';
+import Recipe from "./recipe-book.json";
 
 function App() {
-  return ( 
-    <div 
-      style={{
-        margin: "auto",
-        width: 800,
-        paddingTop: "1rem",
-      }}
-    >
-      <h1
-        className="title"
-      >
-        Main Title 
-      </h1>
-      <table 
-        width="100%"
-      >
-        <thead>
-          <tr>
-            <th>Recipe</th>
-            <th>Type</th>
-            <th
-              width="200%"
-            >
-              Link
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Chocolate Cake</td>
-            <td>Desert</td>
-            <td>
-              <a 
-                href="https://www.youtube.com/watch?v=UsyrMndrICQ&list=PLOc2A52hKlB6OTtLo0UQRKfd5-Fbn9iK2&index=341"
-                target="_blank">
-                yt-Link
-              </a>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
+
+	return ( 
+		<div 
+			style={{
+				margin: "auto",
+				width: 800,
+				paddingTop: "1rem",
+			}}
+		>
+			<h1
+				className="title"
+			>
+				Main Title 
+			</h1>
+			<table 
+				width="100%"
+			>
+				<thead>
+					<tr>
+						<th>Recipe</th>
+						<th>Type</th>
+						{/* <th>Summary</th> */}
+						<th>Ingredients</th>
+						<th
+							width="200%"
+						>
+							Link
+						</th>
+					</tr>
+				</thead>
+				<tbody>
+					{Recipe.map(Recipe => (
+						<tr
+							key={Recipe.id}
+						>
+							<td>{Recipe.name}</td>
+							<td>{Recipe.type}</td>
+							<td className="summary">
+							 <td>
+								{Recipe.summary.map( summary => (
+									<tr>
+										<td>items.prep-time</td>
+										<td>items.cook-time</td>
+										<td>items.additional-time</td>
+										<td>items.total-time</td>
+									</tr>
+								))} 
+							</td>
+							</td>
+							{/* <td> {Recipe.ingredients.slice(0,2)} </td> */}
+							<td>
+								<a 
+									href={Recipe.link}
+									target="_blank">
+									Full Recipe
+								</a>
+							</td>
+					</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
 }
 
 export default App;
