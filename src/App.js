887685c9d699928c8a7b81d4cd73f040aaed7580
Contesini <gcontesini@gmail.com>
2022-12-27 11:55:18 -0500

    "React Hooks handling"

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React from "react";
+import PropTypes from "prop-types";
 import './App.css';
 import Recipe from "./recipe-book.json";
 
+// React Components
+
+const Ingredients = ({_ingredients}) => (
+	<td> {_ingredients.map((_ingredients_) => 
+		<li key={_ingredients_.toString()}> {_ingredients_} </li>
+	)}
+	</td>
+);
+
+const Summary = ({_summary}) => (
+	<td>
+		<li>Prep time:{_summary.cook_time} min</li>
+		<li>Cook time:{_summary.prep_time} min</li>
+		<li>Additional time:{_summary.additional_time} min</li>
+		<li>Total time:{_summary.total_time} min</li>
+		<li>Servings:{_summary.servings}</li>
+	</td>
+);
+
 function App() {
+	// React Hook
+	// state is the current state of the hook
+	// setState is the setter of the variable state 
+	const [state, setState] = React.useState("");
 
 	return ( 
 		<div 
@@ -12,57 +36,44 @@ function App() {
 				paddingTop: "1rem",
 			}}
 		>
-			<h1
-				className="title"
-			>
-				Main Title 
+			<h1 className="title">
+				Recipe Search 
 			</h1>
+			<input
+				value={state}
+				onChange={(_state) => setState(_state.target.value)}
+			/>
 			<table 
-				width="100%"
+				width="150%"
 			>
 				<thead>
 					<tr>
-						<th> Recipe </th>
-						<th> Type </th>
-						{/* <th>Summary</th> */}
-						<th width="300%">  Ingredients </th>
-						<th width="200%"> Link </th>
+						<th width="0%"> Recipe </th>
+						<th width="0%"> Type </th>
+						<th width="25%"> Summary</th>
+						<th width="80%"> Ingredients </th>
+						<th width="75%"> Link </th>
 					</tr>
 				</thead>
 				<tbody>
-					{Recipe.map(Recipe => (
+					{Recipe
+						.filter((Recipe) => Recipe.name.includes(state))
+						.slice(0,2)
+						.map(Recipe => (
 						<tr key={Recipe.id}>
 							<td>{Recipe.name}</td>
 							<td>{Recipe.type}</td>
-							{/* <td className="summary">
-							 <td>
-								{Recipe.summary.map( summary => (
-									<tr>
-										<td>items.prep-time</td>
-										<td>items.cook-time</td>
-										<td>items.additional-time</td>
-										<td>items.total-time</td>
-									</tr>
-								))} 
-							</td> */}
-							<td> {Recipe.ingredients.map((ingredients) => 
-								// generate automatic keys
-								<li >{ingredients}</li>
-								)}
-							</td>
+							<Summary _summary={Recipe.summary}/>
+							<Ingredients key={Recipe.ingredients.toString()} _ingredients={Recipe.ingredients}/>
 							<td>
-								<a 
-									href={Recipe.link}
-									target="_blank">
-									Full Recipe
-								</a>
+								<a href={Recipe.link} target="_blank"> Full Recipe </a>
 							</td>
-					</tr>
+						</tr>
 					))}
 				</tbody>
 			</table>
 		</div>
 	);
-}
+};
 
 export default App;
